c3a4d38ce7bc0ac872c487a0d79a2582087b3cd4
Alex.Wang <32781197@qq.com>
2019-12-03 16:02:58 +0800

    1

---
+++ Assets/Script/CombineRenderTexture.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineRenderTexture
+{
+    public int[] mIds;
+
+    public Dictionary<int, Texture2D> textures;
+
+    private Texture2D clearTex;
+    private Material rtMat;
+    private RenderTexture rt;
+
+    private int RTW = 4096;
+    private int RTH = 4096;
+
+    private Vector4 mOffsetScale = new Vector4();
+    public CombineRenderTexture(string path)
+    {
+        init(path);
+    }
+    private int getNumber(string s)
+    {
+        string numStr = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] >= '0' && s[i] <= '9')
+            {
+                numStr += s[i];
+            }
+        }
+        return int.Parse(numStr);
+    }
+    private void init(string path)
+    {
+        Object[] objs = Resources.LoadAll(path);
+        clearTex = new Texture2D(1, 1);
+        clearTex.SetPixel(0, 0, Color.clear);
+        Texture2D t = objs[0] as Texture2D;
+
+        textures = new Dictionary<int, Texture2D>(objs.Length);
+        List<int> keys = new List<int>();
+        for (int i = 0; i < objs.Length; i++)
+        {
+            Texture2D tex = objs[i] as Texture2D;
+
+            int key = getNumber(tex.name);
+            if (keys.Contains(key)) continue;
+
+            keys.Add(key);
+            textures.Add(key, tex);
+        }
+        mIds = keys.ToArray();
+        rtMat = new Material(Shader.Find("NIANHUI/merge"));
+    }
+    public RenderTexture GetCombineRT { get { return rt; } }
+    public RenderTexture CreateCombineRT(out int numX, out int numY)
+    {
+        return CreateCombineRT(mIds, out numX, out numY);
+    }
+    public RenderTexture CreateCombineRT(int[] ids, out int numX, out int numY)
+    {
+        if (rt == null)
+        {
+            rt = new RenderTexture(RTW, RTH, 0, RenderTextureFormat.ARGB32);
+        }
+        numX = Mathf.CeilToInt(Mathf.Sqrt(ids.Length));
+        numY = Mathf.CeilToInt(ids.Length / (float)numX);
+        float _width = RTW / (float)numX;
+        float _height = RTH / (float)numY;
+
+        mOffsetScale.Set(0, 0, 1, 1);
+        rtMat.SetVector("_OffsetScale", mOffsetScale);
+        Graphics.Blit(clearTex, rt, rtMat);
+
+        for (int i = 0; i < numX * numY; i++)
+        {
+            int index = i;
+            if (i >= ids.Length) { index = Random.Range(0, ids.Length); }
+
+            int x = i % numX;
+            int y = i / numX;
+            mOffsetScale.x = x * _width / RTW * 2.0f - 1 + _width / 4096.0f;
+            mOffsetScale.y = y * _height / RTH * 2.0f - 1 + _height / 4096.0f;
+            mOffsetScale.z = _width / RTW;
+            mOffsetScale.w = _height / RTH;
+            rtMat.SetVector("_OffsetScale", mOffsetScale);
+            if (!textures.ContainsKey(ids[index])) { continue; }
+            Graphics.Blit(textures[ids[index]], rt, rtMat);
+        }
+
+        return rt;
+    }
+}
---
+++ Assets/Script/PSCC.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.ParticleSystem;
+
+public class PSCC : MonoBehaviour, UGUIActor
+{
+    public GameObject PSObj;
+    public GameObject BGImageObj;
+
+    private Image BGImage;
+    private Sprite[] SpriteArr;
+    private Material PSMat;
+    private ParticleSystem PS;
+    private RenderTexture rt;
+    private int numTilesX, numTilesY;
+    private CombineRenderTexture crt;
+    private Texture2D GetEmployeePhoto(int id)
+    {
+        return crt != null ? crt.textures[id] : null;
+    }
+    private void SetParticleCount(int count)
+    {
+        MainModule mm = PS.main;
+        mm.maxParticles = count;
+    }
+    private void SetParticleTitleXY(int titleX, int titleY)
+    {
+        TextureSheetAnimationModule tsam = PS.textureSheetAnimation;
+        tsam.numTilesX = numTilesX;
+        tsam.numTilesY = numTilesY;
+    }
+    private int _frame = 0;
+    private float _totalTime = 0;
+    private Sprite ShowSpriteSequence(Sprite[] sprites, int fps, int begin = 0)
+    {
+        //if (_frame == 0) Debug.Log(Time.time);
+        return ShowSpriteSequence(sprites, ref _frame, begin, sprites.Length - 1, fps, Time.deltaTime, ref _totalTime);
+    }
+    private Sprite ShowSpriteSequence(Sprite[] sprites, ref int frame, int minFrame, int maxFrame, int fps, float deltaTime, ref float totalTime)
+    {
+        totalTime += deltaTime;
+        if (totalTime >= (1f / fps - 0.01f))
+        {
+            frame = Mathf.Max(frame, minFrame);
+            frame += 1;
+            if (frame > maxFrame) frame = minFrame;
+            totalTime = 0;
+        }
+        return sprites[frame];
+    }
+    private void Init()
+    {
+        if (PSObj != null)
+        {
+            PS = PSObj.GetComponent<ParticleSystem>();
+            PSMat = PSObj.GetComponent<ParticleSystemRenderer>().material;
+
+            rt = crt.CreateCombineRT(out numTilesX, out numTilesY);
+            PSMat.SetTexture("_MainTex", rt);
+
+            SetParticleCount(numTilesX * numTilesY);
+            SetParticleTitleXY(numTilesX, numTilesY);
+        }
+        if (BGImageObj != null)
+        {
+            BGImage = BGImageObj.GetComponent<Image>();
+            SpriteArr = Resources.LoadAll<Sprite>("BGSequence");
+        }
+    }
+    PhotoLayout pl = new PhotoLayout(1406, 954);
+    void Start()
+    {
+        string path = "PIC";
+
+        crt = new CombineRenderTexture(path);
+
+        Init();
+        SetParticleCount(0);
+        pl.init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BGImage.sprite = ShowSpriteSequence(SpriteArr, 25, 500);
+    }
+    private void OnGUI()
+    {
+        pl.test();
+    }
+    public void UGUI_Start_Click()
+    {
+        SetParticleCount(numTilesX * numTilesY);
+        SetParticleTitleXY(numTilesX, numTilesY);
+    }
+
+    public void UGUI_Stop_Click()
+    {
+        SetParticleCount(0);
+    }
+}
+public interface UGUIActor
+{
+    void UGUI_Start_Click();
+    void UGUI_Stop_Click();
+}
